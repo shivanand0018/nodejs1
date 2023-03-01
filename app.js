3159a7223f0ffe838f01ae725c200a79f08f98e5
shivanand0018 <101448498+shivanand0018@users.noreply.github.com>
2023-03-01 14:49:27 +0530

    creating server

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ console.log("node js");
 const fs=require('fs');
 
 fs.writeFileSync('file.txt','Shivanand')
+
+const http=require('http');
+let server=http.createServer((req,res)=>{
+    console.log(req);
+})
+server.listen(4000)
